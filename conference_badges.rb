e7376ac18e51e8871a3db5a6f790798de924ef30
Steve Cordova <cordovas@bc.edu>
2020-07-14 02:54:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,10 +2,10 @@
 
 # uncomment out from here
 
-# def badge_maker(name)
-#   return "Hello, my name is #{name}."
-# end
-#
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
 # def batch_badge_creator(array)
 #   x = []
 #   array.each do |names|
