0e14b2fb71f41fa153f1ed6675ac46221b7f7456
Steve Cordova <cordovas@bc.edu>
2020-07-14 02:53:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,16 +1,22 @@
 # Write your code here.
-def badge_maker(name)
-  return "Hello, my name is #{name}."
-end
 
-def batch_badge_creator(array)
-  x = []
-  array.each do |names|
-    y = "Hello, my name is #{names}."
-    x.push(y)
-  end
-x
-end
+# uncomment out from here
+
+# def badge_maker(name)
+#   return "Hello, my name is #{name}."
+# end
+#
+# def batch_badge_creator(array)
+#   x = []
+#   array.each do |names|
+#     y = "Hello, my name is #{names}."
+#     x.push(y)
+#   end
+# x
+# end
+
+# uncomment out to here
+
 
 # def batch_badge_creator(attendees)
 #   attendees.map do |attendee|
@@ -18,23 +24,24 @@ end
 #   end
 # end
 
-def assign_rooms(attendees)
-count = 1
-x = []
-attendees.each do |names|
-  y = "Hello, #{names}! You'll be assigned to room #{count}!"
-  count+=1
-  x.push(y)
-end
-x
-end
-
-def printer(attendees)
-  batch_badge_creator(attendees).each do |badge|
-    puts badge
-  end
-
-  assign_rooms(attendees).each do |assignment|
-    puts assignment
-  end
-end
+# uncomment out from here
+# def assign_rooms(attendees)
+# count = 1
+# x = []
+# attendees.each do |names|
+#   y = "Hello, #{names}! You'll be assigned to room #{count}!"
+#   count+=1
+#   x.push(y)
+# end
+# x
+# end
+#
+# def printer(attendees)
+#   batch_badge_creator(attendees).each do |badge|
+#     puts badge
+#   end
+#
+#   assign_rooms(attendees).each do |assignment|
+#     puts assignment
+#   end
+# end
